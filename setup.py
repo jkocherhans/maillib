42be7b6601c471a7acb4d3c5aa3b57768bae4027
Joseph Kocherhans <jkocherhans@mac.com>
2009-11-01 21:02:43 -0600

    Added some more metadata to setup.py so PyPi will show the README.

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+import os
 from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README')).read()
 
 setup(
     name = "maillib",
-    version = "0.1.1",
-    description = "A simplified interface to  Python's email package.",
+    version = "0.1.2",
+    description = "A simplified interface to Python's email package.",
+    long_description = README,
     author = 'Joseph Kocherhans',
     author_email = 'jkocherhans@gmail.com',
+    license='BSD',
+    url='http://github.com/jkocherhans/maillib',
     packages = ['maillib'],
     classifiers = [
         'Development Status :: 3 - Alpha',
